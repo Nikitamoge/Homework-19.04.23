0ee6d74fca60ecbb7f5daf5447f3964bde026db8
Nikei <Nikei@NIKITA>
2023-04-19 00:38:28 +0200

    Add project files.

---
+++ Homework/Homework.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(0, "UKR");
+	cout << "\t2023\n";
+	cout << "\tApril\n";
+	cout << "M  T  W  T  F  S  S\n";
+	cout << "               1  2\n";
+	cout << "3  4  5  6  7  8  9\n";
+	cout << "10 11 12 13 14 15 16\n";
+	cout << "17 18 19 20 21 22 23\n";
+	cout << "24 25 26 27 28 29 30\n"; cout << ""; //First task
+
+
+	cout << "            (                 \n";
+	cout << "               )              \n";
+	cout << "            (                 \n";
+	cout << "          ---||               \n";
+	cout << "        -'   ||               \n";
+	cout << "      -'     ||'-             \n";
+	cout << "    -'           '-           \n";
+	cout << "  -'_______________'-         \n";
+	cout << "     | []   [] |              \n";
+	cout << "     |    __   |              \n";
+	cout << "     |   | .|  |_._._._._.    \n";
+	cout << "     |___|__|__||=|=|=|=|=|   \n";  //Second task
+
+	cout << "  **  **   **       ***    **  **    **       ***     ******        **           \n";
+	cout << "  **  **   **     ** **    ** **     **     ** **       **        **  **         \n";
+	cout << "  ******   **   **   **    ***       **   **   **       **      **       **      \n";
+	cout << "  **  **   ** **     **    ** **     ** **     **       **    ***************    \n";
+	cout << "  **  **   ***       **    **  **    ***       **       **  **               **  \n";  //Third task
+
+
+	cout << "\t*\n      ***\n      *****\n      *******\n      *********\n      ***********\n      *************\n      ***************\n      ****************\n\t*\n";  //Fourth task
+	cout << "My resume:\n   \tIliushchenko Nikita Andreevich;\n    \tI'm 16 years old;\n   \tI'm learning C++ now;\n   \tAlso, i'm studying at IT academy 'STEP'";  //Fifth task
+}
